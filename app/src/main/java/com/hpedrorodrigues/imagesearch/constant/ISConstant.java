d1e8f4b0a4fc88123a9f7d75c0f497a9bd06fccb
Pedro Rodrigues <hs.pedro.rodrigues@gmail.com>
2016-07-31 18:52:33 -0300

    Changing default search

--- app/src/main/java/com/hpedrorodrigues/imagesearch/constant/ISConstant.java
+++ app/src/main/java/com/hpedrorodrigues/imagesearch/constant/ISConstant.java
@@ -4,7 +4,7 @@ import android.os.Environment;
 
 public interface ISConstant {
 
-    String DEFAULT_SEARCH = "tree";
+    String DEFAULT_SEARCH = "cats";
     String DEFAULT_DIRECTORY = Environment.DIRECTORY_DOWNLOADS;
     int IMAGES_PER_PAGE = 15;
     int INITIAL_PAGE = 1;
