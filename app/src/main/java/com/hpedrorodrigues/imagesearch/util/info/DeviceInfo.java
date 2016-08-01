ac12c5886d2c8181368dbbf66baa790346d3000a
Pedro Rodrigues <hs.pedro.rodrigues@gmail.com>
2016-08-01 20:46:52 -0300

    Adding DeviceInfo

---
+++ app/src/main/java/com/hpedrorodrigues/imagesearch/util/info/DeviceInfo.java
@@ -0,0 +1,42 @@
+package com.hpedrorodrigues.imagesearch.util.info;
+
+import android.os.Build;
+
+public class DeviceInfo {
+
+    public String getDetails() {
+        return new StringBuilder()
+                .append("Device details:")
+                .append("\n\n")
+                .append("Device: ")
+                .append(Build.DEVICE)
+                .append("\n")
+                .append("CPU: ")
+                .append(Build.CPU_ABI)
+                .append("\n")
+                .append("Manufacturer: ")
+                .append(Build.MANUFACTURER)
+                .append("\n")
+                .append("Model: ")
+                .append(Build.MODEL)
+                .append("\n")
+                .append("Hardware: ")
+                .append(Build.HARDWARE)
+                .append("\n")
+                .append("Brand: ")
+                .append(Build.BRAND)
+                .append("\n")
+                .append("Tags: ")
+                .append(Build.TAGS)
+                .append("\n")
+                .append("Android version: ")
+                .append(Build.VERSION.RELEASE)
+                .append(" (API ")
+                .append(Build.VERSION.SDK_INT)
+                .append(")")
+                .append("\n\n")
+                .append("---------------------------------------------")
+                .append("\n\n\n\n")
+                .toString();
+    }
+}
